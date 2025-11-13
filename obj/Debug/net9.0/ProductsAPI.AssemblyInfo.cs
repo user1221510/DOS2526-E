77cba22c13a36e536ac641d1b33d521638facd56
FranciscoSilva27 <1241588@isep.ipp.pt>
2025-11-13 19:51:57 +0000

    .

--- obj/Debug/net9.0/ProductsAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01b5e7bb6d7d671938908f356802ac7e79e4fa2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7771b737ec277244631e5a09ecf8f05e9039a595")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
