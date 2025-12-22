54685ad505814b41e45a4988323aa94a52ccb6c7
Vasco Morais Gonçalves <1221510@isep.ipp.pt>
2025-12-22 22:08:19 +0000

    Reparação no deploy do script

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,59 +1,26 @@
 #!/bin/bash
 
-set -e # Parar se houver erro
+# 1. Ler o argumento que vem do Jenkins
+IMAGE_NAME=$1
 
-
-
-echo "A iniciar Deploy de PRODUÇÃO..."
-
-
-
-# --- CONFIGURAÇÃO ---
+# Se o Jenkins não enviar nada, usa um valor por defeito para não dar erro
+if [ -z "$IMAGE_NAME" ]; then
+    IMAGE_NAME="dos2526-api:latest"
+fi
 
 CONTAINER_NAME="dos2526-api-prod"
+HOST_PORT="8050"
 
-IMAGE_NAME="dos2526-api:latest"
-
-# Nome da rede criada pelo docker-compose
-
-NETWORK_NAME="dos2526-e_default" 
-
-DB_HOST="db-1" 
-
-
-
-# 1. Parar versão antiga
+echo "A iniciar Deploy de PRODUÇÃO..."
+echo "Imagem a utilizar: $IMAGE_NAME"
 
 echo "A limpar container antigo..."
-
+# O "|| true" impede que o script falhe se o container não existir
 docker stop $CONTAINER_NAME || true
-
 docker rm $CONTAINER_NAME || true
-
-
-
-# 2. Arrancar nova versão
 
 echo "A arrancar novo container..."
 
+docker run -d --restart unless-stopped -p $HOST_PORT:8080 --name $CONTAINER_NAME $IMAGE_NAME
 
-
-docker run -d \
-
-  --name $CONTAINER_NAME \
-
-  --restart always \
-
-  --network $NETWORK_NAME \
-
-  -p 8050:8080 \
-
-  -e "ASPNETCORE_ENVIRONMENT=Production" \
-
-  -e "ConnectionStrings__DefaultConnection=Server=$DB_HOST;Database=ProductsDB;User Id=sa;Password=GrupoE2526!;TrustServerCertificate=True;" \
-
-  $IMAGE_NAME
-
-
-
-echo " Deploy PROD concluído com sucesso! (Porta 8050)"
+echo "Deploy concluído com sucesso!"
